0e9afa409d29c321054e9a4e141344d18e3ee69e
Петров Владислав Андреевич <vapetrov@nic.etu>
2023-03-31 21:24:43 +0300

    add page not found

---
+++ src/views/PageNotFoundView.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col>
+                <h1>Page Not Found</h1>
+                <h2>404</h2>
+            </v-col>
+        </v-row>
+    </v-container>
+  </template>
+  
+  <script lang="ts">
+  import Vue from 'vue'
+  
+  
+  export default Vue.extend({
+    name: 'PageNotFound',
+  })
+  </script>
